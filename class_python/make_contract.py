596f0b2ed69b3a74bfe7fdb2378e27717d53025f
Jaka Widjaja <36018102+JakaWidjaja@users.noreply.github.com>
2024-05-26 14:45:42 +1000

    Delete class_python directory

--- class_python/make_contract.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class make_contract(ABC):
-	@abstractmethod
-	def futures(self):
-		pass 
-
-	def option(self):
-		pass
--- class_python/old code/make_data.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-class make_data(ABC):
-	def create_data(self):
-		pass
