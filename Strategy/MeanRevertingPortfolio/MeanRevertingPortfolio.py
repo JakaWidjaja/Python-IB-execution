e1975ca2e7b50ac881c983eb498e12c05ac3ef80
Jaka Widjaja <36018102+JakaWidjaja@users.noreply.github.com>
2024-06-17 21:45:19 +1000

    Add files via upload

--- Strategy/MeanRevertingPortfolio/MeanRevertingPortfolio.py
+++ Strategy/MeanRevertingPortfolio/MeanRevertingPortfolio.py
@@ -7,11 +7,12 @@ class MeanRevertingPortfolio:
     def __init__(self):
         pass
 
-    def StockSelection(self, data, lengthTimeSeries, numTopStocks, numStockToUse):
+    def StockSelection(self, data, lengthTimeSeries, numTopStocks, numStockToUse, longShort):
         self.data             = data
         self.lengthTimeSeries = lengthTimeSeries
         self.numTopStocks     = numTopStocks
         self.numStockToUse    = numStockToUse
+        self.longShort        = longShort
         
         #Select the top stocks
         portSelection = PortfolioSelection.PortfolioSelection()
@@ -19,3 +20,25 @@ class MeanRevertingPortfolio:
         
         #Calculate the different stock combinations
         stockCombination = list(topStocks, self.numStockToUse)
+        
+        #Calculate the weights and portfolio time series
+        weights = {}
+        w = PortfolioWeights.PortfolioWeights()
+        for c in stockCombination:
+            stockList = data.loc[:, c]
+            weights[c] = w.BoxTiao(stockList, 
+                                   1,
+                                   [0.5] * self.numStockToUse, 
+                                   self.longShort, 
+                                   self.lengthTimeSeries)
+            
+            sumProduct = stockList * weights[c]
+            portfolioTimeSeries = sumProduct.sum(axis = 1)
+        
+        
+        
+        
+
+            
+
+
