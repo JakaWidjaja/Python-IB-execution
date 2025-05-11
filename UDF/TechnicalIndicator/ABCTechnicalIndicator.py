00e68a2aeb43833784bd40035d3c455dd35d64fd
Jaka Widjaja <36018102+JakaWidjaja@users.noreply.github.com>
2025-05-11 14:09:21 +1000

    Delete UDF directory

--- UDF/TechnicalIndicator/ABCTechnicalIndicator.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ABCTechnicalIndicator(ABC):
-    def __init__():
-        pass
-    
-    @abstractmethod
-    def Calculate(self, timeSeries):
-        pass
